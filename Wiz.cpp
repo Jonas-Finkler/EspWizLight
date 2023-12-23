60f61f23ca3bf849fe0fb02ff88b4781d5c24297
Jonas Finkler <jonas@finkler.me>
2023-12-23 16:47:01 +0100

    adding library files and first example

---
+++ Wiz.cpp
@@ -0,0 +1,248 @@
+#include "Wiz.h"
+
+
+
+int Wiz::discoverLights(Wiz lights[], int maxNumLights){
+    // const char* msg = "{\"method\":\"registration\",\"params\":{\"phoneMac\":\"AAAAAAAAAAAA\",\"register\":false,\"phoneIp\":\"1.2.3.4\",\"id\":\"1\"}}";
+    StaticJsonDocument<JSON_SIZE> command;
+    // command["method"] = "getPilot"; // the getPilot seems to not work when the lights are off
+    // command.createNestedArray("params"); // add empty params:{}
+    command["method"] = "registration";
+    command["params"]["phoneMac"] = "AAAAAAAAAAAA";
+    command["params"]["register"] = false;
+    command["params"]["phoneIp"] = "1.2.3.4";
+    // command["params"]["id"] = "1";
+    WiFiUDP udp;
+    // Already start listenting for response
+    udp.begin(WIZ_PORT);
+    IPAddress broadcastIP = WiFi.localIP();
+    // The broadcast IP ends with 255
+    broadcastIP[3] = 255;
+
+    udp.beginPacket(broadcastIP, Wiz::WIZ_PORT);
+    serializeJson(command, udp);
+    udp.endPacket();
+
+    int iLight = 0;
+    while (true){
+        StaticJsonDocument<JSON_SIZE> response;
+        int responseState = awaitResponse(udp, response);
+        if (responseState != WizResult::SUCCESS) { // did not get any reply todo: differenciate between errors and timeout
+            Serial.println("no success");
+            break; 
+        }
+
+        lights[iLight].ip = udp.remoteIP();
+        lights[iLight].mac = response["result"]["mac"].as<String>();
+
+        Serial.println("Discovered a light.");
+        Serial.print("  Message: ");
+        serializeJson(response, Serial);
+        Serial.println("");
+        Serial.print("  IP: ");
+        Serial.println(lights[iLight].ip);
+        Serial.print("  mac: ");
+        Serial.println(lights[iLight].mac);
+        
+        iLight++;
+        if (iLight >= maxNumLights) {
+            break; // exit loop
+            // otherwise, wait for more packets from other lamps
+            // todo: error handling
+
+        }
+    }
+
+    udp.stop();
+    return iLight;
+
+
+}
+
+WizResult Wiz::sendCommand(StaticJsonDocument<JSON_SIZE> command, StaticJsonDocument<JSON_SIZE> &response){
+    WiFiUDP udp;
+    // Already start listenting for response
+    udp.begin(WIZ_PORT);
+    udp.beginPacket(this->ip, Wiz::WIZ_PORT);
+    serializeJson(command, udp);
+    udp.endPacket();
+    WizResult responseState = awaitResponse(udp, response);
+    udp.stop();
+    return responseState;
+}
+
+WizResult Wiz::awaitResponse(WiFiUDP &udp, StaticJsonDocument<JSON_SIZE> &response){
+    int start = millis();
+    while (start + Wiz::TIMEOUT > millis()){
+        int packetSize = udp.parsePacket();
+        if (packetSize) {
+
+            char packetBuffer[packetSize+1];
+            int len = udp.read(packetBuffer, packetSize);
+            packetBuffer[len] = 0; // terminate string
+            // Serial.println(packetBuffer);
+            
+            // we need this cast here
+            // otherwise, the json lib will work with references to packetBuffer, which will soon go out of scope
+            DeserializationError error = deserializeJson(response, (const char*) packetBuffer);
+            if (error) {
+                // todo: Error handling
+                Serial.println("deserialization error");
+                return WizResult::DESERIALIZATION_ERROR;
+            } else {
+                return WizResult::SUCCESS;
+            }
+        }
+    }
+    Serial.println("timeout");
+    return WizResult::TIMEOUT;
+}
+
+void Wiz::setColor(int r, int g, int b, int c=0, int w=0){
+    config.r = r;
+    config.g = g;
+    config.b = b;
+    config.c = c;
+    config.w = w;
+    config.mode = RGBCW_MODE;
+
+}
+    
+void Wiz::setTemperature(int temperature){
+    config.mode = TEMPERATURE_MODE;
+    config.temperature = temperature;
+}
+
+void Wiz::setState(bool state){
+    config.state = state;
+}
+
+bool Wiz::getState() {
+    return config.state;
+}
+
+void Wiz::setDimming(int dimming){
+    config.dimming = max(min(dimming, DIM_MAX), DIM_MIN);
+}
+
+int Wiz::getDimming() {
+    return config.dimming;
+}
+
+int Wiz::getTemperature() {
+    if (config.mode == TEMPERATURE_MODE) {
+        return config.temperature;
+    } else {
+        return -1;
+    }
+}
+
+WizResult Wiz::pullConfig(){
+    StaticJsonDocument<JSON_SIZE> command;
+    StaticJsonDocument<JSON_SIZE> response;
+    command["method"] = "getPilot";
+    command.createNestedArray("params"); // add empty params:{}
+    // serializeJson(command, Serial);
+    // Serial.println("");
+
+    WizResult sendResult = sendCommand(command, response);
+    if (sendResult == WizResult::SUCCESS) {
+        // Serial.print("Lamp state: ");
+        // serializeJson(response, Serial);
+        // Serial.println("");
+        config.state = response["result"]["state"];
+        config.sceneId = response["result"]["sceneId"];
+        // some scenes (the wakeup one) do not contain a dim value
+        // so we just set it to -1 so we will not send back 0 and cause an error the next time
+        if (response["result"].containsKey("dimming")) {
+            config.dimming = response["result"]["dimming"]; 
+        } else {
+            config.dimming = -1;
+        }
+        if (config.sceneId == 0) {
+            if (response["result"].containsKey("temp")) {
+                config.mode = TEMPERATURE_MODE;
+                config.temperature = response["result"]["temp"];
+            } else if (response["result"].containsKey("r")) {
+                config.mode = RGBCW_MODE;
+                config.r = response["result"]["r"];
+                config.g = response["result"]["g"];
+                config.b = response["result"]["b"];
+                config.c = response["result"]["c"];
+                config.w = response["result"]["w"];
+
+            } else {
+                config.mode = OTHER_MODE;
+            }
+        } else {
+            config.mode = SCENE_MODE;
+        }
+
+
+        return WizResult::SUCCESS;
+    } else {
+        return sendResult;
+    }
+    // todo: error handling
+
+}
+
+WizResult Wiz::pushConfig(){
+    StaticJsonDocument<JSON_SIZE> command;
+    StaticJsonDocument<JSON_SIZE> response;
+
+    command["method"] = "setPilot";
+    command["params"]["state"] = config.state;
+    if (config.dimming >= 0) {
+        command["params"]["dimming"] = config.dimming;
+    }
+    switch (config.mode) {
+    case RGBCW_MODE:
+        command["params"]["r"] = config.r;
+        command["params"]["g"] = config.g;
+        command["params"]["b"] = config.b;
+        command["params"]["c"] = config.c;
+        command["params"]["w"] = config.w;
+        break;
+
+    case TEMPERATURE_MODE:
+        command["params"]["temp"] = config.temperature;
+        break;
+
+    case SCENE_MODE:
+        command["params"]["sceneId"] = config.sceneId;
+        break;
+    }
+
+    WizResult sendResult = sendCommand(command, response);
+    // Serial.print("Command: ");
+    // serializeJson(command, Serial);
+    // Serial.println("");
+    // Serial.print("Response: ");
+    // serializeJson(response, Serial);
+    // Serial.println("");
+    if (sendResult != WizResult::SUCCESS) {
+        return sendResult;
+    }
+    if (response.containsKey("error")) {
+        return WizResult::LIGHT_ERROR;
+        // const char* errorMessage = response["error"]["message"];
+        // int errorCode = response["error"]["code"];
+        // todo: catch the parameter error here
+    }
+
+    if (response["result"].containsKey("success")) {
+        if (response["result"]["success"]) {
+            return WizResult::SUCCESS;
+        } 
+    } 
+    return WizResult::ERROR;
+}
+
+LampConfig Wiz::getConfig() {
+    return config;
+}
+
+IPAddress Wiz::getIP() {
+    return ip;
+}
---
+++ Wiz.h
@@ -0,0 +1,100 @@
+#ifndef Wiz_h
+#define Wiz_h
+#include "Arduino.h" 
+#include <WiFiUdp.h>
+#include "WiFi.h"
+#include <ArduinoJson.h>
+
+// todo: consistent language: lamp/light
+
+enum LampMode {TEMPERATURE_MODE, RGBCW_MODE, SCENE_MODE, OTHER_MODE};
+
+enum WizResult {
+    SUCCESS = 0, 
+    ERROR,
+    TIMEOUT, 
+    DESERIALIZATION_ERROR, 
+    LIGHT_ERROR // When the light sends back an error (might be wrong parameters (out of range))
+}; 
+
+struct LampConfig {
+    LampMode mode = OTHER_MODE;
+    bool state = false;
+    int r = 0;
+    int g = 0;
+    int b = 0;
+    int c = 0;
+    int w = 0;
+    int temperature = 0;
+    int dimming = 0;
+    int sceneId = 0; // 0 means no scene, for other scenes see: https://github.com/sbidy/pywizlight/blob/master/pywizlight/scenes.py
+};
+
+class Wiz{
+private:
+    // todo: use #define here?
+    static const int JSON_SIZE = 512; // memory for json objects
+    static const int WIZ_PORT = 38899;
+    static const int TIMEOUT = 2000;
+    IPAddress ip;
+    String mac; // todo: use char[12] here
+    // const char* mac;
+
+    // Sends a command and waits for the response. Returns 0 on success.
+    WizResult sendCommand(StaticJsonDocument<JSON_SIZE> command, StaticJsonDocument<JSON_SIZE> &response);
+
+    // Waits for a udp response from the light.
+    static WizResult awaitResponse(WiFiUDP &udp, StaticJsonDocument<JSON_SIZE> &response);
+
+    LampConfig config;
+
+public:
+    // todo: these should be read from the bulb
+    static const int DIM_MIN=10;
+    static const int DIM_MAX=100;
+    static const int TEMPERATURE_MIN=2200;
+    static const int TEMPERATURE_MAX=6500;
+
+    // todo: implement constructor from IP
+
+    // Uses a broadcast message to discover all lights in the network.
+    // Returns the number of lights found.
+    static int discoverLights(Wiz* lights, int maxNumLights);
+
+    // Pulls the state of the light into the Wiz object
+    // Returns 0 on success
+    WizResult pullConfig();
+    // Send configuration to the light
+    WizResult pushConfig();
+
+    // Set the color of the light (c and w are the cold and warm white leds)
+    void setColor(int r, int g, int b, int c, int w); // todo: use bytes here
+    // Set the color temperature of the light
+    void setTemperature(int temp);
+    // Turn the light on or off
+    void setState(bool state);
+    // Returns wether the lamp is on or off
+    bool getState();
+    // set the dimming of the light using a value from DIM_MIN to DIM_MAX
+    void setDimming(int dimming);
+    // returns the current dimming of the light
+    int getDimming();
+    // returns the current color temperature of the light or 0 in case an rgb value or a different scene is currently used
+    int getTemperature();
+    // returns the state of the light (remember to call pullConfig before this)
+    LampConfig getConfig();
+
+    IPAddress getIP();
+
+
+    // todo: 
+    // {"method":"getModelConfig","env":"pro","result":{"ps":2,"pwmFreq":1000,"pwmRes":11,"pwmRange":[0,100],"wcr":60,"nowc":1,"cctRange":[2200,2700,6500,6500],"renderFactor":[200,255,150,255,0,0,40,0,0,0],"noSmartConf":0,"drvIface":0}}
+    // {"method":"getUserConfig","env":"pro","result":{"fadeIn":500,"fadeOut":500,"dftDim":100,"opMode":0,"po":false,"minDimming":0,"tapSensor":1}}
+
+};
+
+#endif
+
+
+// getModelConfig
+// getPilot
