2b74e4774f3941e6c0809f9835df1ec79a543921
Jonas Finkler <jonas@finkler.me>
2023-12-24 14:31:58 +0100

    formatting

--- examples/WizLightTest/WizLightTest.ino
+++ examples/WizLightTest/WizLightTest.ino
@@ -2,64 +2,72 @@
 #include <Arduino.h>
 #include <Wiz.h>
 
-// I assume that maximally 8 lights are in the network. Adjust this if you have more.
-const int MAX_NUM_LIGHTS = 8;
+// I assume that maximally 4 lights are in the network. Adjust this if you have
+// more.
+const int MAX_NUM_LIGHTS = 4;
 const char* SSID = "SSID";
-const char* PASSWORD =  "PASSWORD";
+const char* PASSWORD = "PASSWORD";
 
 Wiz lights[MAX_NUM_LIGHTS];
 int numLights;
 
-void setup(){
-    Serial.begin(115200);
-    Serial.print("Connecting to WiFi ");
-    WiFi.begin(SSID, PASSWORD);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(100);
-      Serial.print(".");
-    }
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.print("IP: ");
-    Serial.println(WiFi.localIP());
-    // discover all lights on the network
-    numLights = Wiz::discoverLights(lights, MAX_NUM_LIGHTS);
-    Serial.printf("Discovered %i lights.\n", numLights);
+void setup() {
+  Serial.begin(115200);
+  Serial.print("Connecting to WiFi ");
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
 
-    for(int i=0; i<numLights; i++) {
-      Serial.println(lights[i].getIP());
-      // lights[i].setTemperature(Wiz::TEMPERATURE_MIN);
-      lights[i].setColor(200, 50, 50, 150, 150);
-      lights[i].setDimming(50);
-      lights[i].setState(true);
-      lights[i].pushConfig();
-    } 
-    delay(30000);
-    for(int i=0; i<numLights; i++) {
-      lights[i].setState(false);
-      lights[i].pushConfig();
-    } 
-  // if (nLights <= 0){
-  //   // dont continue
-  //   while(true){
-  //     Serial.println("Error: no lights found");
-  //     delay(1000);
-  //   }
-    
-  // }
-  // for (int i=0; i<nLights; i++){
-  //   lights[i].pullConfig();
-  // }
-  // light = lights[0];
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+
+  // discover all lights on the network
+  numLights = Wiz::discoverLights(lights, MAX_NUM_LIGHTS);
+  Serial.printf("Discovered %i lights.\n", numLights);
+  for (int i = 0; i < numLights; i++) {
+    Serial.printf("-- Light %i --\n", i);
+    Serial.print("  IP: ");
+    Serial.println(lights[i].getIP());
+    lights[i].pullConfig();  // receive config from light
+    LightConfig conf = lights[i].getConfig();
+    Serial.printf("  State: %s\n", conf.state);
+    switch (conf.mode) {
+      case RGBCW_MODE:
+
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  // set all lights to a user defined color for 30 seconds
+  for (int i = 0; i < numLights; i++) {
+    lights[i].setColor(200, 50, 50, 150, 150);
+    lights[i].setDimming(50);
+    lights[i].setState(true);
+    lights[i].pushConfig();
+  }
+  delay(30000);
+
+  // set all lights to a 3000K white for 30 seconds
+  for (int i = 0; i < numLights; i++) {
+    lights[i].setTemperature(3000);
+    lights[i].pushConfig();
+  }
+  delay(30000);
+
+  // turn off all lights
+  for (int i = 0; i < numLights; i++) {
+    lights[i].setState(false);
+    lights[i].pushConfig();
+  }
 }
 
-int loopCounter = 0;
-void loop(){
-  // for(int i=0; i<numLights; i++){
-  //   lights[i].setState(i==loopCounter%numLights);
-  //   lights[i].pushConfig();
-  // }
-
-  delay(10000);
-  loopCounter ++;
+void loop() {
+  // do nothing here
 }
