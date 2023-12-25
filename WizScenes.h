151d746ace8e0f6a70abb7f983e215eaa3bb5480
Jonas Finkler <jonas@finkler.me>
2023-12-25 14:12:02 +0100

    adding scenes header

---
+++ WizScenes.h
@@ -0,0 +1,67 @@
+
+
+// Adapted from pywizlight
+// (https://github.com/sbidy/pywizlight/blob/master/pywizlight/scenes.py) The
+// original code was published under the MIT license. MIT License
+//
+// Copyright (c) 2020 Stephan Traub
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef WizScenes_h
+#define WizScenes_h
+
+enum WizScene {
+  NO_SCENE = 0,
+  OCEAN = 1,
+  ROMANCE = 2,
+  SUNSET = 3,
+  PARTY = 4,
+  FIREPLACE = 5,
+  COZY = 6,
+  FOREST = 7,
+  PASTEL_COLORS = 8,
+  WAKE_UP = 9,
+  BEDTIME = 10,
+  WARM_WHITE = 11,
+  DAYLIGHT = 12,
+  COOL_WHITE = 13,
+  NIGHT_LIGHT = 14,
+  FOCUS = 15,
+  RELAX = 16,
+  TRUE_COLORS = 17,
+  TV_TIME = 18,
+  PLANTGROWTH = 19,
+  SPRING = 20,
+  SUMMER = 21,
+  FALL = 22,
+  DEEPDIVE = 23,
+  JUNGLE = 24,
+  MOJITO = 25,
+  CLUB = 26,
+  CHRISTMAS = 27,
+  HALLOWEEN = 28,
+  CANDLELIGHT = 29,
+  GOLDEN_WHITE = 30,
+  PULSE = 31,
+  STEAMPUNK = 32,
+  RHYTHM = 1000
+};
+
+#endif
